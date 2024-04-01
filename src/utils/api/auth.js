e0fd78fac91b3bd38b53336927e32939a7098ae2
KimSungKwon <sungkwonn@naver.com>
2024-04-01 23:40:17 +0900

    [feature] create oauth login api

---
+++ src/utils/api/auth.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+// 소셜 로그인
+export const requestLogin = async (req, socialType) => {
+  let returnValue;
+
+  await axios
+    .post()
+    .then()
+    .catch();
+
+  return returnValue;
+}
