10fbd9bc24d87423b3297c70211ced6c97a8dbbd
KimSungKwon <sungkwonn@naver.com>
2024-04-03 14:11:59 +0900

    [feat] google login dev

--- src/components/button/GoogleLoginButton.js
+++ src/components/button/GoogleLoginButton.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
+
+import { requestLogin } from '../../utils/api/auth';
 
 const GoogleLoginButtonLayout = styled.div``;
 
 const GoogleLoginButton = () => {
 
+  const responseGooogleLogin = async (code, socialType) => {
+    await requestLogin(code, socialType).then((res) => {
+      
+    })
+  }
+
   const googleLogin = useGoogleLogin({
     onSuccess: (res) => {
-      
-    }
-  })
+      responseGooogleLogin(res.access_token, "GOOGLE");
+    },
+  });
 
   return (
     <GoogleLoginButtonLayout>
+      <button onClick={googleLogin}>Btn</button>
     </GoogleLoginButtonLayout>
   );
 };
